21e8f267d13694133231ded3713fec146492c351
kalyan.chavali <kalyan.chavali@dish.com>
2020-08-05 08:59:25 -0600

    extremely slow pace

--- src/com/interviewPrep/problems/Chap4PrimitiveTypes/Four3ReverseBits.java
+++ src/com/interviewPrep/problems/Chap4PrimitiveTypes/Four3ReverseBits.java
@@ -4,6 +4,24 @@ package com.interviewPrep.problems.Chap4PrimitiveTypes;
 
 public class Four3ReverseBits {
     public static void main(String[] args) {
+        System.out.println(reverseBits(16L));
+    }
 
+    public static long reverseBits(long num){
+        int i = 63, j = 1;
+
+        while(j <= i){
+            int bit1 = (int)((num >>> i) & 1);
+            int bit2 = (int)((num >>> j) & 1);
+
+            if(bit1 != bit2){
+                num ^= (1L << i);
+                num ^= (1L << j);
+            }
+            j++;
+            i--;
+        }
+
+        return num;
     }
 }
